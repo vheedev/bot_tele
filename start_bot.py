b0675b520a692289d8093c93dd1b6e94d84b5ee1
vheedev <vhee.dev@gmail.com>
2025-04-28 09:35:20 +0700

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -1,3 +1,4 @@
+
 import time
 import requests
 
@@ -6,27 +7,26 @@ TOKEN = "7722270521:AAGErN_FlV_7XVq8_yABGdHep-x2tVVEUGo"
 URL_GET_UPDATES = f"https://api.telegram.org/bot7722270521:AAGErN_FlV_7XVq8_yABGdHep-x2tVVEUGo/getUpdates"
 URL_SEND_MESSAGE = f"https://api.telegram.org/bot7722270521:AAGErN_FlV_7XVq8_yABGdHep-x2tVVEUGo/sendMessage"
 
-# Fungsi buat ambil pesan
-def get_updates():
-    response = requests.get(URL_GET_UPDATES)
+def get_updates(offset=None):
+    params = {"timeout": 100, "offset": offset}
+    response = requests.get(f"{URL}/getUpdates", params=params)
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
-# Fungsi buat kirim balasan
 def send_message(chat_id, text):
     payload = {
         "chat_id": chat_id,
         "text": text
     }
-    requests.post(URL_SEND_MESSAGE, data=payload)
+    requests.post(f"{URL}/sendMessage", data=payload)
 
-print("✅ Bot polling dan auto-reply DIMULAI... Tunggu pesan dari Telegram...")
+print("✅ Bot polling dimulai.")
 last_update_id = None
 
 while True:
-    data = get_updates()
+    data = get_updates(offset=last_update_id)
     if data and "result" in data:
         for update in data["result"]:
             update_id = update["update_id"]
@@ -34,11 +34,10 @@ while True:
                 if "message" in update:
                     chat_id = update["message"]["chat"]["id"]
                     text = update["message"]["text"]
-                    print(f"📩 Pesan dari {chat_id}: {text}")
+                    print(f"📩 {chat_id}: {text}")
 
-                    # AUTO REPLY ketika dapat pesan
-                    reply_text = "Halo Babe! 💬 Aku sudah online dan siap kerjain tugas kamu hari ini! 🚀❤️"
+                    reply_text = "Morning Babe! 💬 Aku sudah online siap kerja 🚀❤️"
                     send_message(chat_id, reply_text)
 
-                    last_update_id = update_id
+                last_update_id = update_id + 1
     time.sleep(3)
